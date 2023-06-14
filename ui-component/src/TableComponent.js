847ee8098a98a35fb39866bf9d3e59e48036ed7e
Jordan Walker <jordanswalker28@gmail.com>
2023-06-14 21:41:37 +0100

    added search

--- ui-component/src/TableComponent.js
+++ ui-component/src/TableComponent.js
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TableComponent = ({ data }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  // Filter the data based on the search query
+  const filteredData = data.filter(([label]) =>
+    label.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <table style={{ width: '80%', marginTop: '2rem', textAlign: 'center' }}>
-      <thead>
-        <tr>
-          <th>Label</th>
-          <th>Count</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(([label, count]) => (
-          <tr key={label}>
-            <td>{label}</td>
-            <td>{count}</td>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: '5rem',
+      }}
+    >      <input
+        type="text"
+        placeholder="Search..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        style={{ marginBottom: '1rem' }}
+      />
+
+      <table style={{ width: '80%', marginTop: '2rem', textAlign: 'center' }}>
+        <thead>
+          <tr>
+            <th>Label</th>
+            <th>Count</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {filteredData.map(([label, count]) => (
+            <tr key={label}>
+              <td>{label}</td>
+              <td>{count}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
