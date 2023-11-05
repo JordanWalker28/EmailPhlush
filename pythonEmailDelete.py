94a1f805af8ee6d628170f4309267e880b1ffe23
Jordan Walker <jordanswalker28@gmail.com>
2023-11-05 13:41:43 +0000

    updated some items

--- pythonEmailDelete.py
+++ pythonEmailDelete.py
@@ -6,28 +6,23 @@ from email.header import decode_header
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from threading import Lock
-import os
-from urllib.parse import unquote
+from collections import defaultdict
 
-def search_emails(emails, imap):
-    line_dict = {}
+def search_and_delete_emails(emails, imap):
+    line_dict = search_emails(emails, imap)
 
-    for emailAddress in emails:
-        emailRec = emailAddress.strip()
-        print(f"searching for {emailRec}")
-        messages = imap.search(None, f'FROM "{emailRec}"')
+    for emailAddress, count in line_dict.items():
+        if count < 1:
+            print(f"No Emails for {emailAddress}")
+        else:
+            delete_emails(emailAddress, imap, line_dict)
 
-        # convert messages to a list of email IDs
-        messages = messages[0].split()
-        line_dict[emailAddress] = len(messages)
-
-    return line_dict
 
 def delete_emails(emailAddress, imap, line_dict, permanently_delete):
     print(f"Deleting {line_dict[emailAddress]} Emails for {emailAddress}")
     provider = emailAddress.strip()
     count = 1
-    messages = imap.search(None, f'FROM "{provider}"')
+    status, messages = imap.search(None, f'FROM "{provider}"')
     messages = messages[0].split()
     messageCount = len(messages)
 
@@ -92,10 +87,7 @@ def process_email(imap, email_counts, num, lock, processed_emails, total_emails)
             sender_email = extract_sender_email(raw_email_string)
             if sender_email:
                 with lock:
-                    if sender_email in email_counts:
-                        email_counts[sender_email] += 1
-                    else:
-                        email_counts[sender_email] = 1
+                    email_counts[sender_email] += 1
                     processed_emails[0] += 1
                     percentage = (processed_emails[0] / total_emails) * 100
                     print(f"Processing email {processed_emails[0]}/{total_emails} ({percentage:.2f}% complete)")
@@ -107,15 +99,12 @@ def process_email(imap, email_counts, num, lock, processed_emails, total_emails)
             print(f"Server response: {response}")
 
 def scan_emails(imap):
-    email_counts = {}  # Dictionary to store email addresses and their counts
+    email_counts = defaultdict(int)
     lock = Lock()  # Lock to synchronize access to email_counts and IMAP object
 
     try:
-        imap.select("inbox")
-
-        # Searching for all emails
-        result, data = imap.search(None, "ALL")
-        email_numbers = data[0].split()[:100]  # Limit to the first 100 emails
+        status, data = imap.search(None, 'ALL')
+        email_numbers = data[0].split()[:500]  # Limit to the first 100 emails
         total_emails = len(email_numbers)  # Total number of emails to process
         print(f"Total emails to process: {total_emails}")
 
@@ -123,7 +112,6 @@ def scan_emails(imap):
 
         with ThreadPoolExecutor() as executor:
             futures = []
-
             for num in email_numbers:
                 future = executor.submit(process_email, imap, email_counts, num, lock, processed_emails, total_emails)
                 future.num = num  # Attach email number to future object
@@ -134,7 +122,7 @@ def scan_emails(imap):
                 try:
                     future.result()
                 except Exception as e:
-                    print(f"An error occurred while processing email {num}: {e}")
+                    print(f"An error occurred while processing email {num.decode()}: {e}")
                     with lock:
                         response = getattr(imap, 'response', None)
                     if response:
@@ -146,61 +134,6 @@ def scan_emails(imap):
 
     except Exception as e:
         print(f"An error occurred while scanning emails: {e}")
-
-
-def scan_emails_unsub(imap):
-    subLinksFileName = 'unsubscribeLinks'
-    limit = 0
-    emailCount = 0
-    unsubLinks = []
-
-    subLinksFile = open(subLinksFileName, 'w')
-
-    rv, data = imap.search(None, "TEXT unsubscribe")
-
-    for num in data[0].split():
-        try:
-            if limit != 0 and emailCount > limit:
-                print('Reached the maximum of emails we want to process')
-                break
-
-            print('Processing email id = ', num, '...', sep='')
-
-            rv, data = imap.fetch(num, '(RFC822)')
-            if rv != 'OK':
-                print("ERROR getting message", num)
-                continue
-
-            msg = email.message_from_bytes(data[0][1])
-            sender_email = msg['From']
-
-            if msg.is_multipart():
-                for payload in msg.get_payload():
-                    body = payload.get_payload()
-            else:
-                body = msg.get_payload()
-
-            # Extract the unsubscribe link using regular expressions
-            unsubscribe_links = re.findall(r'href="(.*?)"', body)
-            for link in unsubscribe_links:
-                if 'unsubscribe' in link:
-                    decoded_link = unquote(link)
-                    if decoded_link not in unsubLinks:
-                        unsubLinks.append(decoded_link)
-                        print('Found new link to unsubscribe from: ', decoded_link)
-                        subLinksFile.write(f"Sender: {sender_email}\nUnsubscribe Link: {decoded_link}\n\n")
-
-        except Exception as e:
-            print('We encountered an exception but we decided to keep going.')
-            print('The exception was: ', e)
-            pass
-
-        emailCount += 1
-
-    print('Emails processed: ', emailCount)
-    print('Unsubscribe links found: ', len(unsubLinks))
-
-    subLinksFile.close()
 
 def main(username, password, method, permDelete=False):
     # create an IMAP4 class with SSL
@@ -220,9 +153,6 @@ def main(username, password, method, permDelete=False):
 
     if method == "scan":
         scan_emails(imap)
-
-    if method == "unsubscribe":
-        scan_emails_unsub(imap)
 
     close_connection(imap)
 
